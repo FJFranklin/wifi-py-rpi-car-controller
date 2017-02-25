1c9db3826f44cf466f14ffb917e164e19dfbe952
FJFranklin <fjf@alinameridon.com>
2017-02-25 20:44:27 +0000

    circles example now works with touch-events

--- static/circles.js
+++ static/circles.js
@@ -1,14 +1,31 @@
 /* Red circle moves in response to button-down events; X & Y values update according to mouse position events
  */
-function red_circle (event, type) {
+function red_circle (event, bMouse, type) {
     var svg_box = document.getElementById ("foreground");
     var value_x = document.getElementById ("value_x");
     var value_y = document.getElementById ("value_y");
 
     var rect = svg_box.getBoundingClientRect ();
 
-    var x = event.clientX - rect.x;
-    var y = event.clientY - rect.y;
+    var x = rect.width / 2;
+    var y = rect.height / 2;
+
+    if (bMouse) {
+	if (event.buttons || event.button) {
+	    x = event.clientX - rect.x;
+	    y = event.clientY - rect.y;
+	}
+    } else {
+	if ((type == "start") || (type == "move")) {
+	    if (event.touches.length) {
+		var touch = event.touches[0];
+		x = touch.pageX - rect.left;
+		y = touch.pageY - rect.top;
+	    }
+	}
+	event.preventDefault ();
+	event.stopPropagation ();
+    }
 
     if (x < 0) {
 	x = 0;
@@ -26,31 +43,39 @@ function red_circle (event, type) {
     value_x.innerHTML = (2 * x / rect.width  - 1).toFixed (3);
     value_y.innerHTML = (2 * y / rect.height - 1).toFixed (3);
 
-    if ((event.buttons === 0) && (event.button === 0)) {
-	x = rect.width / 2;
-	y = rect.height / 2;
-    }
-
     var c_red = document.getElementById ("circle_red");
 
     c_red.setAttribute ("cx", x.toFixed (3));
     c_red.setAttribute ("cy", y.toFixed (3));
 }
 
-function e_down (event) {
-    red_circle (event, "down");
+function e_m_down (event) {
+    red_circle (event, true, "down");
 }
-function e_up (event) {
-    red_circle (event, "up");
+function e_m_up (event) {
+    red_circle (event, true, "up");
 }
-function e_over (event) {
-    red_circle (event, "over");
+function e_m_over (event) {
+    red_circle (event, true, "over");
 }
-function e_move (event) {
-    red_circle (event, "move");
+function e_m_move (event) {
+    red_circle (event, true, "move");
 }
-function e_out (event) {
-    red_circle (event, "out");
+function e_m_out (event) {
+    red_circle (event, true, "out");
+}
+
+function e_t_start (event) {
+    red_circle (event, false, "start");
+}
+function e_t_cancel (event) {
+    red_circle (event, false, "cancel");
+}
+function e_t_end (event) {
+    red_circle (event, false, "end");
+}
+function e_t_move (event) {
+    red_circle (event, false, "move");
 }
 	
 /* Asynchronous request from server, sending and receiving text
@@ -90,10 +115,15 @@ function get_started () {
 
     var stick = document.getElementById ("foreground");
 
-    stick.addEventListener ("mousedown", e_down, false);
-    stick.addEventListener ("mouseup",   e_up,   false);
-    stick.addEventListener ("mouseover", e_over, false);
-    stick.addEventListener ("mousemove", e_move, false);
-    stick.addEventListener ("mouseout",  e_out,  false);
+    stick.addEventListener ("mousedown", e_m_down, false);
+    stick.addEventListener ("mouseup",   e_m_up,   false);
+    stick.addEventListener ("mouseover", e_m_over, false);
+    stick.addEventListener ("mousemove", e_m_move, false);
+    stick.addEventListener ("mouseout",  e_m_out,  false);
+
+    stick.addEventListener ("touchstart",  e_t_start,  false);
+    stick.addEventListener ("touchcancel", e_t_cancel, false);
+    stick.addEventListener ("touchend",    e_t_end,    false);
+    stick.addEventListener ("touchmove",   e_t_move,   false);
 }
 window.onload = get_started;
